82abd2b428b17661ebd9d1e5b5e34a3cf71ad24f
pratikw007 <waghmarepratik287@gmail.com>
2024-02-15 23:27:11 +0530

    Laying the Foundation

--- 4-Laying-the-foundation/App.js
+++ 4-Laying-the-foundation/App.js
@@ -2,57 +2,41 @@ import React from "react";  // getting it from node modules
 import ReactDOM from "react-dom/client";
 
 
-// <div class="header">
-//     <ul>
-//         <li>Home</li>
-//         <li>About Us</li>
-//         <li>Contact  Us</li>
-//     </ul>
-// </div>
-
-
-    
-    //React.createElement => Object => HTML(DOM) 
-
-// const container = React.createElement(
-//     "div", 
+// this is Actual React Code 
+// const heading = React.createElement(
+//     "h1",
 //     {
-//         id: "container", 
-//         Hello : "world", 
+//         id: "title",
+//         key:"h2",     
 //     },
-//     [React.createElement(
-//         "h1", 
-//         {
-//             id: "title",
-//             key: "h1",
-//         },
-//         "Heading 1 for parcel" 
-//         ) , 
-//         React.createElement("ul",{},        
-//             [React.createElement(
-//                 "li",
-//                 {},
-//                 "About us"
-//             ),React.createElement("ul",{},        
-//             [React.createElement(
-//                 "li",
-//                 {},
-//                 "Contact Us"
-//             ),React.createElement("ul",{},        
-//             [React.createElement(
-//                 "li",
-//                 {},
-//                 "Home"
-//             )]) 
-// ]
+//     "React is working"
+// );
 
-// )
 
-// console.log(heading);
+// JSX  - JavaScript XML 
+// Facebook Developer Develop JSX
+const heading1 =(
+     <h1 id="title" key="h2">
+        Namste React
+        </h1>
+)
+
+//React Component
+// Functional Component -- New
+//  ---> Nothing but the Javascript Function. (Normal Function)
+ const HeaderComponent = () => {
+       return( 
+       <div>
+         {heading1}
+         {/* We can do mathematical Operation in that just link Console */}
+         {2+1}  
+         <h1>Hello Pratik</h1>
+         <h2>Ram</h2>
+         </div>
+)
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading1);
+root.render(<HeaderComponent />);
 
-//Passing the react element inside the root
-
-//async defer 
-root.render(container);
