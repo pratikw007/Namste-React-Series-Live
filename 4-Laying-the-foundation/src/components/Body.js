2cf1e80476345d8a674bad7bf3c5c7043d7dc62e
pratikw007 <waghmarepratik287@gmail.com>
2024-02-21 16:04:11 +0530

    Commit

--- 4-Laying-the-foundation/src/components/Body.js
+++ 4-Laying-the-foundation/src/components/Body.js
@@ -12,6 +12,17 @@ import { useState } from 'react';
 // What is useState? --
 
 
+//
+//e.g.:-> "uncle ji hotel" ---> it will filter ji from "uncle ji hotel"
+function filterData(searchText, restaurants){
+  const filterData = restaurants.filter((res) => 
+        res.data.name
+        .includes(searchText)
+        );
+        return filterData;
+}
+
+
 const Body = () => {
   // create a variable in JS
   // const searchTxt = "KFC";
@@ -19,7 +30,7 @@ const Body = () => {
 const [restaurants,setRestaurants] = useState(restaurantList);
 
   // create a variable in React
-   const [searchInput,setSearchText] = useState("Search"); // to create state variable--> return the array of element
+   const [searchText,setSearchInput] = useState(""); // to create state variable--> return the array of element
     return (
       <>
       <div className="-search-container">
@@ -27,25 +38,28 @@ const [restaurants,setRestaurants] = useState(restaurantList);
        type='text' 
        className='search-input' 
        placeholder='Search for a restraunt...'
-       value={searchInput}
+       value={searchText}
        onChange={(e) => {
         // e.target.values --> whatever you write in input
-        setSearchText(e.target.value);
+        setSearchInput(e.target.value);
        }}
        />
 
-
-       <button className='search-btn'
+       <button className="search-btn"
        onClick={() =>{ 
         // Need to filter the data (Restralist data)
-        // filterData();
-       }}
-       >Search</button>
+        const data = filterData(searchText, restaurants); //Most important line EVER
+        // searchText -->  search the searchText in list of restarent (restaurants)     
+        // update the state -- > Restrarent deails {Next step}
+        setRestaurants(data);
+       }}>
+        Search
+        </button>
       </div>
 
       <div className="restraunt-list">
          {
-           restaurantList.map((res) => {
+           restaurants.map((res) => {
              return <RestrauntCard {...res.data}  key={res.data.id}/>;
             })
           }
