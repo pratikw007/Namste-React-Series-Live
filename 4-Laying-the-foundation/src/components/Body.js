a79e4acc49c9155a8c8a6b5f0329ad1e178c76c6
pratikw007 <waghmarepratik287@gmail.com>
2024-02-27 15:14:02 +0530

    useEffect Hooks

--- 4-Laying-the-foundation/src/components/Body.js
+++ 4-Laying-the-foundation/src/components/Body.js
@@ -1,11 +1,10 @@
 // props --> Properties -->Passing some data into component
 
-
-
 import  React from 'react';
 import { restaurantList } from "../components/config.js"
 import RestrauntCard from './RestrauntCard.js';
-import { useState } from 'react';
+import { useState , useEffect} from 'react';
+import Shimmer from  './Shimmer.js  '
 
 // what  is state ?
 // what is React hooks ? --> funnction
@@ -14,6 +13,7 @@ import { useState } from 'react';
 
 //
 //e.g.:-> "uncle ji hotel" ---> it will filter ji from "uncle ji hotel"
+
 function filterData(searchText, restaurants){
   const filterData = restaurants.filter((res) => 
         res.data.name
@@ -27,12 +27,39 @@ const Body = () => {
   // create a variable in JS
   // const searchTxt = "KFC";
   // const searchClick = false;
-const [restaurants,setRestaurants] = useState(restaurantList);
+const [restaurants,setRestaurants] = useState([]);
 
-  // create a variable in React
-   const [searchText,setSearchInput] = useState(""); // to create state variable--> return the array of element
-    return (
-      <>
+// create a variable in React
+const [searchText,setSearchInput] = useState(""); // to create state variable--> return the array of element
+// console.log(restaurants);
+
+// [] ==> empty dependancy array => once after render 
+//  dep array [searchText] => once after initial render + everytime after render (my searchText changes);
+
+useEffect(()=>{
+  //API Call
+   getRestraunts();
+  console.log("Hello");
+    },[searchText]);
+async function getRestraunts(){
+      const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.28844139999999&lng=77.993135999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+      //it return
+      const json = await data.json();
+      console.log(json);
+      // optional chaining
+      setRestaurants(json?.data?.card[2]?.data?.data?.cards);
+}
+  
+  // console.log("render");
+
+
+  // Conditional Rendering
+  //if restraunt is empty = >   Shimmer UI
+  //if restraunt has data = >  Actual Data UI
+
+return(restaurants.length===0)? <Shimmer/> : (
+
+  <>
       <div className="-search-container">
        <input 
        type='text' 
@@ -40,10 +67,10 @@ const [restaurants,setRestaurants] = useState(restaurantList);
        placeholder='Search for a restraunt...'
        value={searchText}
        onChange={(e) => {
-        // e.target.values --> whatever you write in input
-        setSearchInput(e.target.value);
-       }}
-       />
+         // e.target.values --> whatever you write in input
+         setSearchInput(e.target.value);
+        }}
+        />
 
        <button className="search-btn"
        onClick={() =>{ 
@@ -69,4 +96,4 @@ const [restaurants,setRestaurants] = useState(restaurantList);
   };
   
   
-export  default Body;
+export default Body;
